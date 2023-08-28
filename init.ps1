48e44b0eaf24035819a37153dfd9cf3ae567a053
Jaroslav Svoboda (Krtek) <jaroslav.svoboda@elostech.cz>
2023-08-28 23:39:07 +0200

    Fix argocd app

--- init.ps1
+++ init.ps1
@@ -2,3 +2,7 @@ helm repo add argo https://argoproj.github.io/argo-helm
 kubectl create namespace argocd
 helm upgrade --install --namespace  argocd  argocd argo/argo-cd -f .\infra\values\argocd.yaml
 kubectl apply -f root.yaml
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | TODO: Get from base 64
+
+kubectl port-forward service/argocd-server -n argocd 8080:443
